7172ab4bdbbe4cd8f6739a7a219718b57a0e0348
Railson25 <railsonsantiago123@gmail.com>
2021-12-07 12:17:30 -0300

    Initial

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  useFonts
+} from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme';
+import { AppRoutes } from './src/routes/app.routes';
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
+
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,107 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import React, { useEffect, useState } from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard'
+import {
+    Container,
+    Header, HighlightCards,
+    Icon, LogoutButton, Photo,
+    Title, Transaction, TransactionList,
+    User,
+    UserGreeating, UserInfo,
+    UserName, UserWrapper
+} from './styles'
+
+export interface DataListProps extends TransactionCardProps {
+    id: string
+}
+
+export function Dashboard() {
+    const [data, setData] = useState<DataListProps[]>([])
+
+
+    async function loadTransaction() {
+        const dataKey = '@gofinaces:transactions'
+        const response = await AsyncStorage.getItem(dataKey)
+        const transactions = response ? JSON.parse(response) : []
+
+        const transactionsFormated: DataListProps[] = transactions
+            .map((item: DataListProps) => {
+                const amount = Number(item.amount).toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                });
+
+                const date = Intl.DateTimeFormat('pt-BR', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: '2-digit'
+                }).format(new Date(item.date));
+
+                return {
+                    id: item.id,
+                    name: item.name,
+                    amount,
+                    type: item.type,
+                    category: item.category,
+                    date
+                }
+            })
+        setData(transactionsFormated)
+    }
+
+    useEffect(() => {
+        loadTransaction()
+    }, [])
+
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/47870113?v=4' }} />
+                        <User>
+                            <UserGreeating>Olá,</UserGreeating>
+                            <UserName>Railson</UserName>
+                        </User>
+                    </UserInfo>
+                    <LogoutButton onPress={() => { }}>
+                        <Icon name="power" />
+                    </LogoutButton>
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard
+                    type='up'
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 16 de abril"
+                />
+            </HighlightCards>
+            <Transaction>
+                <Title>Listagem</Title>
+
+                <TransactionList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+
+            </Transaction>
+        </Container>
+    )
+}
